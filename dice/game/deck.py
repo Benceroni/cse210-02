a14346d4a32a602ffb54023705d95e33f6c2f833
jadenmcc <jadenmccarrey@gmail.com>
2022-05-07 11:53:55 -0600

    Created own versions

--- dice/game/deck.py
+++ dice/game/deck.py
@@ -1,45 +1,33 @@
 import random
 
 
-# 1) Add the class declaration. Use the following class comment.
+# 1) Add the class declaration.
 class Deck:
-    """A small cube with a different number of spots on each of its six sides.
+    """A stack of individual cards. Each card contains a single value between 1-13 inclusive.
 
-    The responsibility of Die is to keep track of the side facing up and calculate the points for 
-    it.
+    The responsibliity of the Deck is to provide a Current Card value and to store the value of the Last Card played.
    
     Attributes:
-        value (int): The number of spots on the side facing up.
-        points (int): The number of points the die is worth.
-        x, y (int): The screen X and Y coordinates where the die will display.
+        last_value (int): The number value of the last card played.
     """
 
-# 2) Create the class constructor. Use the following method comment.
-    def __init__(self, param_x = 0, param_y = 0):
-        """Constructs a new instance of Die with a value and points attribute. 
-        Sets x,y to passed value, or 0,0 if none set.
+# 2) Create the class constructor.
+    def __init__(self):
+        """Constructs a new instance of Deck with a Current Card attribute. 
 
         Args:
-            self (Die): An instance of Die.
+            self (Deck): An instance of Deck.
         """
-        self.last_card = 0
         self.current_card = random.randint(1,13)
+        self.last_card = random.randint(1,13)
 
-# 3) Create the roll(self) method. Use the following method comment.
+# 3) Create the draw(self) method.
     def draw(self):
-        """Generates a new random value.
+        """Stores the new Last Card value and generates a new Current Card value.
         
         Args:
             self (Deck): An instance of Deck.
         """
         self.last_card = self.current_card
         self.current_card = random.randint(1,13)
-
-
-
-
-    def display(self):
-
-        print(self.current_card)
-        return
 
